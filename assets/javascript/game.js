4e4d05913083a69b74de4bd3d51e1c3616b32165
Carson Curry <carsoncurry52@gmail.com>
2018-08-25 16:46:33 -0500

    final changes made

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,61 +1,80 @@
-// Make a game where the computer randomly generates a letter, 
-    // and the user has to guess that letter
+// Make a psychic game where the user has to guess a letter
 
-// Step 1: Display the text, "Guess what letter I'm thinking of"
+// Make an array of choices using the alphabet
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-// Step 2: Display a counter that tracks wins
+// create variables to track wins, losses, guesses, etc. 
+var wins = 0;
+var losses = 0;
+var guesses = 10;
+var guessesLeft = 10;
+var lettersGuessed = [];
 
-// Step 3: Display a counter that tracks losses
-
-// Step 4: Display a counter that marks number of guesses left
-
-// Step 5: Display a counter that marks guesses made so far
-
-// Step 6: Make it so the "win" and "loss" counters go up dependent on user effectiveness
-
-// Step 7: Automatically refresh the game after a win or loss
-
-var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"];
-
-var wins= 0;
-var losses= 0;
-var guessesL= 10;
-var guessesM= [];
-
+// create a variable to remove the instructions
 var directionsText = document.getElementById("directions-text");
-var guessesLeftText = document.getElementById("guessesleft-text");
-var guessesMadeText = document.getElementById("guessesmade-text");
-var userChoiceText = document.getElementById("userchoice-text");
-var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text");
+// var chosenLetter = null;
 
+// randomize the computer's guess every time
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+// create a function to update the guesses left
+var updateGuessesLeft = function() {
+
+    document.querySelector('#guessesleft-text').innerHTML = "Guesses Left: " + guessesLeft;
+    };
+
+// create a function to update the chosen letter that must be guessed
+var updateChosenLetter = function() {
+
+    this.chosenLetter = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
+    };
+
+// create a function to mark the guesses made by the user so far
+var updateGuessesSoFar = function() {
+
+    document.querySelector('#lettersguessed-text').innerHTML = "Guesses Made So Far: " + lettersGuessed.join(', ');
+    };
+
+// create a function to reset the game each time
+    var reset = function() {
+    totalGuesses = 10;
+    guessesLeft = 10;
+    lettersGuessed = [];
+
+    updateChosenLetter();
+    updateGuessesLeft();
+    updateGuessesSoFar();
+    }
+
+    updateChosenLetter();
+    updateGuessesLeft();
+
+// create an event to sense the user's keystrokes
 document.onkeyup = function(event) {
+    guessesLeft--;
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-    var userGuess = event.key.toLowerCase();
-    var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
-    guessesM.push(userGuess);
+    lettersGuessed.push(userGuess);
+    updateGuessesLeft();
+    updateGuessesSoFar();
 
-    if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c") || (userGuess === "d") || (userGuess === "e") || (userGuess === "f") || (userGuess === "g") || (userGuess === "h") || (userGuess === "i") || (userGuess === "j") || (userGuess === "k") || (userGuess === "l") || (userGuess === "m") || (userGuess === "n") || (userGuess === "o") || (userGuess === "p") || (userGuess === "q") || (userGuess === "r") || (userGuess === "s") || (userGuess === "t") || (userGuess === "u") || (userGuess === "v") || (userGuess === "w") || (userGuess === "x") || (userGuess === "y") || (userGuess === "z")) {
-    if (userGuess === computerGuess) {
-        wins++;
-        alert("You suck!");
-        guessesL= 10;
-        guessesM.length= 0;
-    } else if (guessesL = 0) {
-        losses++;
-        alert("You're lucky!");
-        guessesL= 10;
-        guessesM.length = 0;
-    } else if (userGuess !== computerGuess) {
-        guessesL--;
+    if (guessesLeft > 0){
+
+        if (userGuess == chosenLetter){
+        wins++
+        document.querySelector('#wins-text').innerHTML = "Wins: " + wins;
+        alert("You truly are a mutant! The answer was " + chosenLetter);
+        reset();
+    } 
+
+    } else if(guessesLeft == 0){
+    losses++;
+    document.querySelector('#losses-text').innerHTML = "Losses: " + losses;
+    alert("It seems I was incorrect in considering you for the X-Men. The answer was " + chosenLetter);
+    reset();
     }
 
     directionsText.textContent = "";
 
-    userChoiceText.textContent = "You chose: " + userGuess;
-    guessesMadeText.textContent = "Guesses Made: " + guessesM;
-    guessesLeftText.textContent = "Guesses Left: " + guessesL;
-    winsText.textContent = "Wins: " + wins;
-    lossesText.textContent = "Losses " + losses;
-    }
 };
